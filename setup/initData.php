8506aeec789db99f077470a4a30e81ac1a556a44
Gaël PORT <gael.port@rbs.fr>
2011-06-16 15:50:21 +0200

    [FIX] initData.php

--- setup/initData.php
+++ setup/initData.php
@@ -6,7 +6,7 @@ class bookmarks_Setup extends object_InitDataSetup
 {
 	public function install()
 	{
-		// $this->executeModuleScript('init.xml');
+		$this->executeModuleScript('init.xml');
 	}
 
 	/**
