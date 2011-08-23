b2d8fd86ce09f41b3f077825eb68e748d2c2db68
Franck STAUFFER <franck.stauffer@rbs.fr>
2011-08-23 10:10:48 +0200

    New class injection comptability

--- lib/services/CategoryfolderService.class.php
+++ lib/services/CategoryfolderService.class.php
@@ -17,7 +17,7 @@ class bookmarks_CategoryfolderService extends generic_FolderService
 	{
 		if (self::$instance === null)
 		{
-			self::$instance = self::getServiceClassInstance(get_class());
+			self::$instance = new self();
 		}
 		return self::$instance;
 	}
