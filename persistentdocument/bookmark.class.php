361f68b311c91162f33cc98040ff2a536894916f
Eric HAUSWALD <eric.hauswald@rbs.fr>
2011-08-24 12:18:07 +0200

    [NEW] Indexation refactoring

--- persistentdocument/bookmark.class.php
+++ persistentdocument/bookmark.class.php
@@ -3,22 +3,8 @@
  * Class where to put your custom methods for document bookmarks_persistentdocument_bookmark
  * @package modules.bookmarks.persistentdocument
  */
-class bookmarks_persistentdocument_bookmark extends bookmarks_persistentdocument_bookmarkbase implements indexer_IndexableDocument
+class bookmarks_persistentdocument_bookmark extends bookmarks_persistentdocument_bookmarkbase
 {
-	/**
-	 * @return indexer_IndexedDocument
-	 */
-	public function getIndexedDocument()
-	{
-		$indexedDoc = new indexer_IndexedDocument();
-		$indexedDoc->setId($this->getId());
-		$indexedDoc->setDocumentModel('modules_bookmarks/bookmark');
-		$indexedDoc->setLabel($this->getLabel());
-		$indexedDoc->setLang(RequestContext::getInstance()->getLang());
-		$indexedDoc->setText(f_util_StringUtils::htmlToText($this->getDescriptionAsHtml()));
-		return $indexedDoc;
-	}
-	
 	/**
 	 * @return string
 	 */
